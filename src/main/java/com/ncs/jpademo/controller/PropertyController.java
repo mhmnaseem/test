dd3fd1951da691ec4d14b822b01ed69c52c3341b
Mahmud Naseem <mhmnaseem@gmail.com>
2022-12-21 10:06:45 +0530

    init-2

--- src/main/java/com/ncs/jpademo/controller/PropertyController.java
+++ src/main/java/com/ncs/jpademo/controller/PropertyController.java
@@ -10,7 +10,7 @@ public class PropertyController {
 
     @GetMapping("")
     public String index() {
-        return "Hello World Git";
+        return "Hello World";
     }
 
     @PostMapping("/save")
